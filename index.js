6cc3dc3eb4485005ca43b7a702c8102e00aa5258
ezerdemello <ezerdemello@gmail.com>
2019-01-17 01:26:17 -0200

    realizando renovacao de senha quando necessario

--- index.js
+++ index.js
@@ -1,21 +1,49 @@
 const AWS = require('aws-sdk')
+const _ = require('lodash')
 
-var params = {
+const ClientId = process.env.COGNITO_CLIENT_ID
+const UserPoolId = process.env.COGNITO_USER_POOL_ID
+const USERNAME = 'ezer.mello'
+const PASSWORD = 'xxxxxxxxxxx' 
+
+const params = {
     AuthFlow: 'ADMIN_NO_SRP_AUTH',
-    ClientId: 'xxxxxxxxxxx3vguj0bu526snj',
-    UserPoolId: 'us-east-1_xxxxxxxxxxx',
+    ClientId,
+    UserPoolId,
     AuthParameters: {
-        USERNAME: 'xxxxxxxxxxx',
-        PASSWORD: 'xxxxxxxxxxx'
+        USERNAME,
+        PASSWORD,
     }
 }
 
-var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
-
+const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
 cognitoidentityserviceprovider.adminInitiateAuth(params, function(err, data) {
     if (err) {
         console.log(err, err.stack)
     } else {
         console.log(data)
+        if (data) {
+            const { ChallengeName, Session,  ... resto } = data
+            if (ChallengeName === 'NEW_PASSWORD_REQUIRED') {
+                
+                const paramsToAuthChallenge = { 
+                    ChallengeName: 'NEW_PASSWORD_REQUIRED', 
+                    ChallengeResponses: {
+                        USERNAME,
+                        PASSWORD,
+                        'NEW_PASSWORD': PASSWORD
+                    },
+                    UserPoolId,
+                    ClientId,
+                    Session
+                }
+                cognitoidentityserviceprovider.adminRespondToAuthChallenge(paramsToAuthChallenge, function(err, data) {
+                    if (err) console.log(err, err.stack) // an error occurred
+                    else     console.log('respondToAuthChallenge-sucesso',data)           // successful response
+                })
+            }
+        }
     }
 })
+
+
