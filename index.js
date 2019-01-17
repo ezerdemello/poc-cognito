0c5c343c4d2b43329b834d2fa68725670a3e8d9d
ezerdemello <ezerdemello@gmail.com>
2019-01-16 22:41:34 -0200

    removendo os valores

--- index.js
+++ index.js
@@ -2,23 +2,20 @@ const AWS = require('aws-sdk')
 
 var params = {
     AuthFlow: 'ADMIN_NO_SRP_AUTH',
-    ClientId: '2al36bn9cdmr3vguj0bu526snj',
-    UserPoolId: 'us-east-1_jcvpWEq0W',
+    ClientId: 'xxxxxxxxxxx3vguj0bu526snj',
+    UserPoolId: 'us-east-1_xxxxxxxxxxx',
     AuthParameters: {
-        USERNAME: 'ezer.mello',
-        PASSWORD: 'Ez1nh0Surf12!@',
-        SECRET_HASH: 's2tv60p64j27tqsvdfggq49llk9pb1kjb75r36b6s7ujcoifq49'
+        USERNAME: 'xxxxxxxxxxx',
+        PASSWORD: 'xxxxxxxxxxx'
     }
-};
+}
 
 var cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider()
 
 cognitoidentityserviceprovider.adminInitiateAuth(params, function(err, data) {
-
     if (err) {
-        console.log(err, err.stack);
+        console.log(err, err.stack)
     } else {
-        console.log(data);
+        console.log(data)
     }
-    
-});
+})
